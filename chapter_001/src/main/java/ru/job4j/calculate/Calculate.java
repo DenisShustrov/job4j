a8e973d0a25b6b91b2ef7157f4daece0f2ba25a2
DenisShustrov <denisshustroff@yandex.ru>
2018-10-27 11:51:36 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+ * Class Calculate 
+ * @author shustrov
+ * @since 27.10.2018
+ * @version 1
+ */
+
 public class Calculate {
+	/**
+	 * Main.
+	 * @param args - args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
