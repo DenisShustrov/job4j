bd87ee4da58eb1e93c2d5b747efc7f01ecf96aac
DenisShustrov <denisshustroff@yandex.ru>
2018-11-03 20:18:14 +0300

    6.8. Удаление дубликатов в массиве.

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,42 @@
+package ru.job4j.array;
+import java.util.Arrays;
+/**
+ * Class ArrayDuplicate.
+ * @author dshustrov
+ * @version 1
+ * @since 03.11.2018
+ */
+public class ArrayDuplicate {
+
+    /**
+     * Метод удаляет дубликаты в массиве.
+     *
+     * @param array массив строк.
+     * @return массив строк без дубликатов.
+     */
+    public String[] remove(String[] array) {
+        int count = 0;
+        for (int i = 0; i < array.length / 2; i++) {
+            for (int j = i; j < array.length - 1; j++) {
+                if (array[i].equals(array[j + 1])) {
+                    array[j + 1] = "повтор";
+
+                }
+            }
+        }
+        for (int i = array.length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j].equals("повтор")) {
+                    array[j] = array[j + 1];
+                    array[j + 1] = "повтор";
+                }
+            }
+        }
+        for (String x : array) {
+            if (!x.equals("повтор")) {
+               count++;
+            }
+        }
+         return Arrays.copyOf(array, count);
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ * @author dshustrov (denisshustroff@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ArrayDuplicateTest {
+
+    /**
+     * * Test ArrayDuplicate.
+     */
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
+        ArrayDuplicate arrayDuplicate = new ArrayDuplicate();
+        String[] startingLine = {"Привет", "Мир", "Привет", "Супер", "Мир"};
+        String[] result = arrayDuplicate.remove(startingLine);
+        String[] expect = {"Привет", "Мир", "Супер"};
+        assertThat(result, is(expect));
+    }
+}
