d9554d09d426f33e52bd3d67d98f3204548e7286
DenisShustrov <denisshustroff@yandex.ru>
2018-11-26 22:45:19 +0300

    2. Очередь с приоритетом на LinkedList

--- chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -20,25 +20,19 @@ public class PriorityQueue {
      * @param task задача
      */
     public void put(Task task) {
-        if (tasks.size() == 0) {
-            tasks.addFirst(task);
-        } else {
-            for (int i = 0; i < tasks.size(); i++) {
-                if (tasks.get(i).getDesc().equals(task.getDesc())) {
-                    tasks.remove(i);
-                    break;
-                }
-            }
-            for (int y = 0; y < tasks.size(); y++) {
-                if (tasks.get(y).getPriority() > task.getPriority()) {
-                    tasks.add(y, task);
-                    break;
-                } else {
-                    tasks.addLast(task);
-                    break;
-                }
-            }
-        }
+       if (tasks.size() > 0) {
+          for (int i = 0; i < tasks.size(); i++) {
+              if (tasks.get(i).getPriority() > task.getPriority()) {
+                  tasks.add(i, task);
+                  break;
+              }else if (i == tasks.size() - 1) {
+                  tasks.addLast(task);
+                  break;
+              }
+          }
+       } else {
+           tasks.add(task);
+       }
     }
     public Task take() {
         return this.tasks.poll();
