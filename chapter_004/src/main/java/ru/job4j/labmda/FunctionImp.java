41ffcb15b168540273a24dd2eca37b4105122e16
DenisShustrov <denisshustroff@yandex.ru>
2018-12-04 23:20:09 +0300

    1. Подсчет функции в диапазоне.

---
+++ chapter_004/src/main/java/ru/job4j/labmda/FunctionImp.java
@@ -0,0 +1,36 @@
+package ru.job4j.labmda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * Class FunctionImp.
+ *
+ * @author dshustrov
+ * @version 1
+ * @since 04.12.2018
+ */
+public class FunctionImp {
+
+    List<Double> diapason(int start, int end, Function<Double, Double> func) {
+        List<Double> value = new ArrayList<>();
+        for (int index = start; index != end; index++) {
+            value.add(func.apply((double) index));
+        }
+        return value;
+    }
+
+    public static void main(String[] args) {
+        FunctionImp functionImp = new FunctionImp();
+        functionImp.diapason(2, 52,
+                index -> index);
+
+        functionImp.diapason(2, 52,
+                index -> index * index);
+
+        functionImp.diapason(2, 52,
+                Math::log);
+    }
+
+}
