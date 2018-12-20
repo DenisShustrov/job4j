6a1f0ca4c60ece273edb59b20fc863b078b44110
DenisShustrov <denisshustroff@yandex.ru>
2018-12-20 21:57:58 +0300

    Рабочая версия 2.0!!!

---
+++ chess/src/main/java/ru/job4j/chess/firuges/black/PawnBlack.java
@@ -0,0 +1,33 @@
+package ru.job4j.chess.firuges.black;
+
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+import ru.job4j.chess.firuges.Pawn;
+
+/**
+ * @author Petr Arsentev (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class PawnBlack extends Pawn {
+    private final Cell position;
+
+    public PawnBlack(final Cell position) {
+        this.position = position;
+    }
+
+    @Override
+    public Cell position() {
+        return this.position;
+    }
+
+    @Override
+    public Cell[] way(Cell source, Cell dest) {
+        return pawnWay(source, dest);
+    }
+
+    @Override
+    public Figure copy(Cell dest) {
+        return new PawnBlack(dest);
+    }
+}
