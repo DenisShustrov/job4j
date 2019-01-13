bf14739f1aa92280b37549a13bd6e98ed2f30e29
DenisShustrov <denisshustroff@yandex.ru>
2019-01-13 13:45:45 +0300

    2. Не перекрывать equals hashCode

--- chapter_005/src/main/java/ru/job4j/map/User.java
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Class User.
@@ -55,5 +58,33 @@ public class User {
      */
     public Calendar getBirthday() {
         return birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                +
+                "name='" + name + '\''
+                +
+                ", children=" + children
+                +
+                ", birthday=" + birthday.getTime()
+                +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        User one = new User("Denis", 2, new GregorianCalendar(1983, 3, 24));
+        User two = new User("Denis", 2, new GregorianCalendar(1983, 3, 24));
+        Map<User, Integer> map = new HashMap<>();
+        map.put(one, 100);
+        map.put(two, 150);
+        for (Map.Entry<User, Integer> entry : map.entrySet()) {
+            User key = entry.getKey();
+            Object value = entry.getValue();
+            System.out.println(key + " - " + value);
+
+        }
+
     }
 }
