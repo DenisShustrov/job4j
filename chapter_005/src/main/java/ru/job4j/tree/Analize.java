dd2b83508ea3a0e0a08e83bbeb487148d20fe2cf
DenisShustrov <denisshustroff@yandex.ru>
2019-01-19 15:20:16 +0300

    Статистику по коллекции.

--- chapter_005/src/main/java/ru/job4j/tree/Analize.java
+++ chapter_005/src/main/java/ru/job4j/tree/Analize.java
@@ -1,7 +1,6 @@
 package ru.job4j.tree;
 
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * Class Analize.
@@ -19,28 +18,48 @@ public class Analize {
      */
     public Info diff(List<User> previous, List<User> current) {
         Info in = new Info();
-        // we equalize the length of the collections
-        if (previous.size() > current.size()) {
-            for (int i = 0; i <= previous.size() - current.size(); i++) {
-                current.add(null);
-            }
-        } else {
-            for (int i = 0; i <= current.size() - previous.size(); i++) {
-                previous.add(null);
+//        // we equalize the length of the collections
+//        if (previous.size() > current.size()) {
+//            for (int i = 0; i <= previous.size() - current.size(); i++) {
+//                current.add(null);
+//            }
+//        } else {
+//            for (int i = 0; i <= current.size() - previous.size(); i++) {
+//                previous.add(null);
+//            }
+//        }
+//        // main loop
+//        for (int i = 0; i < previous.size(); i++) {
+//            for (int j = 0; j < previous.size(); j++) {
+//                if (previous.get(i) != null && previous.get(i).equals(current.get(j)) && !previous.get(i).name.equals(current.get(j).name)) {
+//                    in.changed++;
+//                }
+//            }
+//            if (previous.get(i) != null && !current.contains(previous.get(i))) {
+//                in.deleted++;
+//            }
+//            if (current.get(i) != null && !previous.contains(current.get(i))) {
+//                in.added++;
+//            }
+//        }
+        Map<Integer, User> map = new HashMap<>();
+        for (User user : previous) {
+            map.put(user.id, user);
+        }
+
+        for (User user : current) {
+            if (!map.containsValue(user)) {
+                in.added++;
             }
         }
-        // main loop
-        for (int i = 0; i < previous.size(); i++) {
-            for (int j = 0; j < previous.size(); j++) {
-                if (previous.get(i) != null && previous.get(i).equals(current.get(j)) && !previous.get(i).name.equals(current.get(j).name)) {
+        for (Map.Entry<Integer, User> entry : map.entrySet()) {
+            for (User user : current) {
+                if (entry.getValue().id == user.id && !entry.getValue().name.equals(user.name)) {
                     in.changed++;
                 }
             }
-            if (previous.get(i) != null && !current.contains(previous.get(i))) {
+            if (!current.contains(entry.getValue())) {
                 in.deleted++;
-            }
-            if (current.get(i) != null && !previous.contains(current.get(i))) {
-                in.added++;
             }
         }
         return in;
@@ -79,5 +98,24 @@ public class Analize {
         int changed;
 
         int deleted;
+    }
+
+    public static void main(String[] args) {
+        Analize an = new Analize();
+        List<Analize.User> previous = new ArrayList<>();
+        previous.add(new Analize.User(123, "Denis"));
+        previous.add(new Analize.User(124, "Ivan"));
+        previous.add(new Analize.User(125, "Max"));
+        previous.add(new Analize.User(126, "Alex"));
+        previous.add(new Analize.User(127, "Dima"));
+        List<Analize.User> current = new ArrayList<>();
+        current.add(new Analize.User(123, "DenisNew"));
+        current.add(new Analize.User(124, "Ivan"));
+        current.add(new Analize.User(127, "DimaNew"));
+        current.add(new Analize.User(128, "New1"));
+        Analize.Info in = an.diff(previous, current);
+        System.out.println(in.added);
+        System.out.println(in.deleted);
+        System.out.println(in.changed);
     }
 }
