9fbcd36931d51f313eec80d6e90b8e4e5dc7e91c
DenisShustrov <denisshustroff@yandex.ru>
2019-01-19 18:05:29 +0300

    1. Пин-понг.

---
+++ chapter_006/src/main/java/ru/job4j/pingpong/RectangleMove.java
@@ -0,0 +1,37 @@
+package ru.job4j.pingpong;
+
+import javafx.scene.shape.Rectangle;
+
+public class RectangleMove implements Runnable {
+    private final Rectangle rect;
+    private int limitX;
+    private int limitY;
+
+    public RectangleMove(Rectangle rect, int limitX, int limitY) {
+        this.rect = rect;
+        this.limitX = limitX;
+        this.limitY = limitY;
+    }
+
+    @Override
+    public void run() {
+        int x = 1;
+        int y = 1;
+        while (true) {
+            this.rect.setX(this.rect.getX() + x);
+            this.rect.setY(this.rect.getY() + y);
+            if (rect.getX() >= limitX || rect.getX() <= 0) {
+                x = -x;
+            }
+            if (rect.getY() >= limitY || rect.getY() <= 0) {
+                y = -y;
+            }
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
