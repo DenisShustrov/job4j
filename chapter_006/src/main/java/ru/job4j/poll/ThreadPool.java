fe2b00a584d81a92195aea58a893ea3ff961d25d
DenisShustrov <denisshustroff@yandex.ru>
2019-01-27 16:23:28 +0300

    1. Реализовать ThreadPool

---
+++ chapter_006/src/main/java/ru/job4j/poll/ThreadPool.java
@@ -0,0 +1,68 @@
+package ru.job4j.poll;
+
+import ru.job4j.producer.SimpleBlockingQueue;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Class ThreadPool.
+ *
+ * @author dshustrov
+ * @version 1
+ * @since 27.01.2019
+ */
+public class ThreadPool {
+    /**
+     * size of poll.
+     */
+    int size = Runtime.getRuntime().availableProcessors();
+    /**
+     * threads poll.
+     */
+    private final List<Thread> threads = new LinkedList<Thread>();
+    /**
+     * tasks to perform.
+     */
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(5);
+
+    /**
+     * Method adds tasks.
+     *
+     * @param job to be done.
+     */
+    public void work(Runnable job) {
+        try {
+            tasks.offer(job);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Method starts threads.
+     */
+    public void shutdown() throws InterruptedException {
+        for (int i = 0; i < size; i++) {
+            threads.add(new Thread(() -> {
+                try {
+                    tasks.poll().run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }));
+        }
+        for (Thread thread : threads) {
+            thread.start();
+            Thread.sleep(5000);
+        }
+        System.out.println("Количество нитей " + threads.size());
+
+    }
+
+    public int getSizeTasks() {
+        return tasks.getsize();
+    }
+
+}
+
---
+++ chapter_006/src/test/java/ru/job4j/poll/ThreadPoolTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.poll;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ThreadPoolTest {
+
+    private ThreadPool tp;
+
+    @Before
+    public void beforeTest() {
+        tp = new ThreadPool();
+        tp.work(() -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println("Doing something 1");
+            }
+
+        });
+
+        tp.work(() -> {
+            for (int i = 0; i < 150; i++) {
+                System.out.println("Doing something 2");
+            }
+
+        });
+    }
+
+    @Test
+    public void whenRunThreadThatPoolWorks() throws InterruptedException {
+        tp.shutdown();
+        assertThat(tp.getSizeTasks(), is(0));
+    }
+}
