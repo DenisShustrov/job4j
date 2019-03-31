ee7717b096a03325068e715f3335fbae717e8696
DenisShustrov <denisshustroff@yandex.ru>
2019-03-31 21:43:04 +0300

    4. Ajax Jquery

--- chapter_008/src/main/java/ru/job4j/frontend/TableAddServlet.java
+++ chapter_008/src/main/java/ru/job4j/frontend/TableAddServlet.java
@@ -1,5 +1,10 @@
 package ru.job4j.frontend;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,25 +12,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 
 public class TableAddServlet extends HttpServlet {
 
-    private final Map<String, String> map = new ConcurrentHashMap<>();
+    private final List<Object> list = new ArrayList<>();
+
+    private static final Logger LOG = LogManager.getLogger(TableAddServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/json");
-        PrintWriter pw = new PrintWriter(new OutputStreamWriter(resp.getOutputStream(), "ISO-8859-1"), true);
-//        Этот код не работает, что я только не делал!!! Данные не добавляются в мапу!!!
-//        ObjectMapper mapper = new ObjectMapper();
-//        String json = mapper.writeValueAsString(map);
-        pw.append(map.get("str"));
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(resp.getOutputStream(), StandardCharsets.ISO_8859_1), true);
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(list);
+        pw.append(json);
         pw.flush();
-        map.clear();
-        pw.close();
+        list.clear();
     }
 
     @Override
@@ -37,10 +42,13 @@ public class TableAddServlet extends HttpServlet {
         }
         assert reader != null;
         reader.close();
-//        ObjectMapper mapper = new ObjectMapper();
-//        map = mapper.readValue(str.toString(), new TypeReference<Map<String, String>>() {
-//        });
-        map.put("str", str.toString());
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            List<Object> temp = mapper.readValue(str.toString(), new TypeReference<List<Object>>() {
+            });
+            list.addAll(temp);
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
     }
-
 }
