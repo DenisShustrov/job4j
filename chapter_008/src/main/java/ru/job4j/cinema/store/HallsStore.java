9dfd5912e30447737646250d800961822bfb1ace
DenisShustrov <denisshustroff@yandex.ru>
2019-04-15 00:11:39 +0300

    Сервис - Кинотеатр

--- chapter_008/src/main/java/ru/job4j/cinema/store/HallsStore.java
+++ chapter_008/src/main/java/ru/job4j/cinema/store/HallsStore.java
@@ -6,12 +6,14 @@ import org.apache.logging.log4j.Logger;
 import ru.job4j.cinema.model.Visitor;
 import ru.job4j.crud.store.DbStore;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * Class HallsStore.
@@ -28,19 +30,80 @@ public class HallsStore implements AutoCloseable, Store {
     private static final HallsStore INSTANCE = new HallsStore();
 
     private HallsStore() {
-        SOURCE.setDriverClassName("org.postgresql.Driver");
-        SOURCE.setUrl("jdbc:postgresql://localhost:5432/users");
-        SOURCE.setUsername("postgres");
-        SOURCE.setPassword("1477556");
-        SOURCE.setMinIdle(5);
-        SOURCE.setMaxIdle(10);
-        SOURCE.setMaxOpenPreparedStatements(100);
+        try (InputStream in = HallsStore.class.getClassLoader().getResourceAsStream("halls.properties")) {
+            Properties config = new Properties();
+            assert in != null;
+            config.load(in);
+            System.out.println(config.getProperty("driver-class-name"));
+            SOURCE.setDriverClassName(config.getProperty("driver-class-name"));
+            SOURCE.setUrl(config.getProperty("url"));
+            SOURCE.setUsername(config.getProperty("username"));
+            SOURCE.setPassword(config.getProperty("password"));
+            SOURCE.setMinIdle(5);
+            SOURCE.setMaxIdle(10);
+            SOURCE.setMaxOpenPreparedStatements(100);
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
+        String crtb = "CREATE TABLE if not exists halls (\n"
+                +
+                "id serial PRIMARY KEY,\n"
+                +
+                "range INTEGER,\n"
+                +
+                "place INTEGER,\n"
+                +
+                "status VARCHAR(200),\n"
+                +
+                "representation VARCHAR(200)\n"
+                +
+                ")";
+        String ins = "INSERT INTO halls (range, place, status, representation) VALUES \n"
+                +
+                "(1, 1, 'free', 'Ряд 1, Место 1'), \n"
+                +
+                "(1, 2, 'free', 'Ряд 1, Место 2'), \n"
+                +
+                "(1, 3, 'free', 'Ряд 1, Место 3'), \n"
+                +
+                "(2, 4, 'free', 'Ряд 2, Место 4'), \n"
+                +
+                "(2, 5, 'free', 'Ряд 2, Место 5'), \n"
+                +
+                "(2, 6, 'free', 'Ряд 2, Место 6'), \n"
+                +
+                "(3, 7, 'free', 'Ряд 3, Место 7'), \n"
+                +
+                "(3, 8, 'free', 'Ряд 3, Место 8'), \n"
+                +
+                "(3, 9, 'free', 'Ряд 3, Место 9')";
+        String crtbac = "CREATE TABLE if not exists accounts (\n"
+                +
+                "user_id serial PRIMARY KEY,\n"
+                +
+                "user_name character varying(200),\n"
+                +
+                "phone character varying(200),\n"
+                +
+                "place character varying(200)\n"
+                +
+                ")";
+        try (Connection connection = SOURCE.getConnection();
+             PreparedStatement create = connection.prepareStatement(crtb);
+             PreparedStatement insert = connection.prepareStatement(ins);
+             PreparedStatement createac = connection.prepareStatement(crtbac)) {
+            create.executeUpdate();
+            createac.executeUpdate();
+            insert.executeUpdate();
+        } catch (SQLException e) {
+            LOG.error(e.getMessage(), e);
+        }
+
     }
 
     public static HallsStore getInstance() {
         return INSTANCE;
     }
-
 
     @Override
     public void close() throws Exception {
@@ -86,7 +149,7 @@ public class HallsStore implements AutoCloseable, Store {
     }
 
     public void addVisitor(Visitor visitor) {
-        String insert = "INSERT INTO accounts (username, phone, place) VALUES (?, ?, ?)";
+        String insert = "INSERT INTO accounts (user_name, phone, place) VALUES (?, ?, ?)";
         String update = "UPDATE halls SET status = 'bought' WHERE representation = ?";
         try (Connection connection = SOURCE.getConnection();
              PreparedStatement in = connection.prepareStatement(insert);
