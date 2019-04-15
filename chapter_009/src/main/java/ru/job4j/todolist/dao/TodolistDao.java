1c475149b29637a9a140008784d34d1e9d483504
DenisShustrov <denisshustroff@yandex.ru>
2019-04-15 20:12:19 +0300

    2. Лямбды и шаблон wrapper

--- chapter_009/src/main/java/ru/job4j/todolist/dao/TodolistDao.java
+++ chapter_009/src/main/java/ru/job4j/todolist/dao/TodolistDao.java
@@ -1,13 +1,14 @@
 package ru.job4j.todolist.dao;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.query.Query;
-import ru.job4j.todolist.model.Item;
-import ru.job4j.todolist.utils.HibernateSessionFactoryUtil;
+        import org.hibernate.Session;
+        import org.hibernate.SessionFactory;
+        import org.hibernate.Transaction;
+        import org.hibernate.query.Query;
+        import ru.job4j.todolist.model.Item;
+        import ru.job4j.todolist.utils.HibernateSessionFactoryUtil;
 
-import java.util.List;
+        import java.util.List;
+        import java.util.function.Function;
 
 public class TodolistDao {
 
@@ -16,19 +17,26 @@ public class TodolistDao {
     public TodolistDao() {
     }
 
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sessionFactory.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            return command.apply(session);
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            tx.commit();
+            session.close();
+        }
+    }
+
     public void save(Item item) {
-        Session session = sessionFactory.openSession();
-        Transaction tx1 = session.beginTransaction();
-        session.save(item);
-        tx1.commit();
-        session.close();
+        this.tx(session -> session.save(item));
     }
 
     public List<Item> findAll() {
-        Session session = sessionFactory.openSession();
-        List<Item> items = (List<Item>) session.createQuery("from Item").list();
-        session.close();
-        return items;
+        return this.tx(session -> session.createQuery("from Item").list());
     }
 
     public void changeStatus(List<String> list) {
@@ -45,9 +53,6 @@ public class TodolistDao {
     }
 
     public List<Item> findAllItemWhenDoneTrue() {
-        Session session = sessionFactory.openSession();
-        List<Item> items = (List<Item>) session.createQuery("from Item where done = true").list();
-        session.close();
-        return items;
+        return this.tx(session -> session.createQuery("from Item where done = true").list());
     }
 }
